2ae1bd7ff438e4d0eb185a2014260c7702c57ab3
Greg Pappas <gnpappas@proton.me>
2024-03-28 11:36:05 -0700

    Add fixed width with ability to scroll on x-axis if username is too long

--- src/ServersAndChans.tsx
+++ src/ServersAndChans.tsx
@@ -1,4 +1,5 @@
 import { emit } from '@tauri-apps/api/event';
+import { SERVER_CHAN_LIST_STYLE, SERVER_CHAN_STYLE } from './style';
 
 export type SACServers = Record<string, string[]>;
 export interface SACProps {
@@ -14,23 +15,24 @@ export interface SACSelectEvent {
   payload: SACSelect
 };
 
+
 function emitSelect(server: string, channel: string = "") {
   emit("nhex://servers_and_chans/select", { server, channel })
 }
 
 export default function ServersAndChans(props: SACProps) {
   return (
-    <div className='border'>
+    <div className={SERVER_CHAN_STYLE}>
       {Object.entries(props.servers).map(([serverName, chans]) => {
         return (
-          <div id={`server_${serverName}`}>
-            <h3 onClick={(e) => emitSelect(serverName)}>{serverName}</h3>
+          <button id={`server_${serverName}`} className={SERVER_CHAN_LIST_STYLE}>
+            <h3 className='font-bold' onClick={(e) => emitSelect(serverName)}>{serverName}</h3>
             {chans.map((channel) => (
               <p id={`chan_${channel.replace("#", "_")}`} onClick={(e) => emitSelect(serverName, channel)}>
                 {channel}
               </p>
             ))}
-          </div>
+          </button>
         )
       })}
     </div>
