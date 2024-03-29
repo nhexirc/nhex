f97db00d20801369665013987ae70e05f8392400
Greg Pappas <gnpappas@proton.me>
2024-03-29 11:16:21 -0700

    Add base responsive capabilities. Still needs lots of work

--- src/style.ts
+++ src/style.ts
@@ -1,5 +1,5 @@
 //APP
-export const ROOT_STYLE = "flex flex-col justify-center space-between bg-zinc-900 text-zinc-300 h-dvh scrollbar-thumb-zinc-400 scrollbar-track-zinc-700"
+export const ROOT_STYLE = "flex flex-col bg-zinc-900 text-zinc-300 h-dvh scrollbar-thumb-zinc-400 scrollbar-track-zinc-700"
 
 //MENU
 export const MENU_STYLE = "flex gap-2 justify-center border-b mb-8 p-2"
@@ -8,16 +8,25 @@ export const MENU_STYLE = "flex gap-2 justify-center border-b mb-8 p-2"
 export const USER_INPUT = "w-full bg-zinc-900 border-b border-t p-1 focus:outline-none placeholder:font-bold placeholder-zinc-300"
 export const USERNAME_STYLE = "italic font-bold"
 export const USER_MESSAGE_STYLE = "pl-2"
-export const MESSAGE_BOX = "w-[730px] h-[600px] overflow-y-auto px-2 scrollbar-thin"
+export const MESSAGE_BOX = "md:min-w-96 md:h-[600px] overflow-y-auto px-2 scrollbar-thin"
 
 //MAINVIEW
-export const MAINVIEW_STYLE = "max-w-5xl mx-auto"
-export const SERVER_FORM_STYLE = "flex flex-col gap-2 border p-2"
-export const SERVER_FORM_BLOCK_STYLE = "flex gap-2"
+export const MAINVIEW_STYLE = "max-w-6xl border mx-auto"
+export const SERVER_FORM_STYLE = "flex flex-col gap-2 border-b p-2"
+export const SERVER_FORM_BLOCK_STYLE = "flex flex-col md:flex-row gap-2"
 export const SERVER_INPUT_STYLE = "bg-zinc-800 placeholder-zinc-400 border p-1 focus:outline-none"
 
-//CHANNELNAMES + SERVER WINDOW
-export const SERVER_CHAN_USER_PANEL_STYLE = "w-48 border-b border-x px-2 overflow-x-hidden overflow-y-auto scrollbar-thin h-[634px]"
+//SERVER + MESSAGEBOX + NAMES
+export const SERV_MSG_NAMES_PANEL_STYLE = "flex flex-col md:flex-row"
+
+//CHANNEL NAMES + SERVER WINDOW
+export const SERVER_CHAN_USER_PANEL_STYLE = "min-w-48 border-b px-2 overflow-x-hidden overflow-y-auto scrollbar-thin h-24 md:h-[634px]"
+
+//SERVER WINDOW
+export const SERVER_PANEL_STYLE = "border-r text-right"
+
+//CHANNEL NAMES
+export const NAMES_PANEL_STYLE = "border-l"
 
 //FOOTER
 export const FOOTER_STYLE = "text-center p-2 border-t mt-8"
