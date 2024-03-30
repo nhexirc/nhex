76cd81258609d2a264e7c01d8d02d4c1f7b01840
Greg Pappas <gnpappas@proton.me>
2024-03-29 18:11:55 -0700

    Split up inputs into different divs for layout

--- src/Connect.tsx
+++ src/Connect.tsx
@@ -1,64 +1,59 @@
-import { SERVER_FORM_BLOCK_STYLE, SERVER_FORM_STYLE, SERVER_INPUT_STYLE } from "./style";
+import { SERVER_FORM_BLOCK_STYLE, SERVER_FORM_STYLE, SERVER_INPUT_STYLE, TLS_ACTIVE_STYLE, TLS_BUTTON_STYLE, TLS_INACTIVE_STYLE } from "./style";
 
-const Connect = ({ setNick, setServer, setPort, port, setTLS, tls, setChannels, connectFunction }) => {
+const Connect = ({ setNick, setServer, setPort, port, handleTLS, tls, setChannels, connect }) => {
+  //TLS is a div and not a button due to it stealing enter from connect button. It has to do with form rules. As a side-effect and a bonus, the tls handler does not need preventDefault. Just pretend it's a button ok!?!
   return (
-    <form
-      className={SERVER_FORM_STYLE}
-      onSubmit={(e) => {
-        e.preventDefault();
-        connectFunction();
-      }}
-    >
-      <div className={SERVER_FORM_BLOCK_STYLE}>
-        <input
-          id="nick-input"
-          className={SERVER_INPUT_STYLE}
-          onInput={(e) => setNick(e.currentTarget.value)}
-          placeholder="Nickname"
-          autoFocus
-        />
-        <input
-          id="server-input"
-          className={SERVER_INPUT_STYLE}
-          onInput={(e) => setServer(e.currentTarget.value)}
-          placeholder="Server"
-        />
-        <input
-          id="port-input"
-          className={SERVER_INPUT_STYLE}
-          onInput={(e) => {
-            const intVal = Number.parseInt(e.currentTarget.value);
-            if (!Number.isNaN(intVal) && Number.isInteger(intVal) && intVal < 65536) {
-              setPort(e.currentTarget.value);
-            }
-            else {
-              e.currentTarget.value = port;
-            }
-          }}
-          placeholder="Port"
-        />
-        <input
-          id="tls-input"
-          type="checkbox"
-          checked={tls}
-          onChange={(e) => setTLS(!tls)}
-        />
-        <label htmlFor="tls-input">TLS</label>
-      </div>
-      <div className={SERVER_FORM_BLOCK_STYLE}>
-        <input
-          id="channels"
-          className={SERVER_INPUT_STYLE}
-          onInput={(e) => setChannels(e.currentTarget.value)}
-          placeholder="#nhex ##programming"
-        />
-        <button
-          type="submit"
-          onClick={(e) => (e.currentTarget.textContent = 'Disconnect')} //needs wiring
-          className="border px-2 py-1"
-        >Connect</button>
-      </div>
-    </form>
+    <>
+      <p className="text-8xl italic text-center pb-8">nHEX</p>
+      <form
+        className={SERVER_FORM_STYLE}
+        onSubmit={connect}
+      >
+        <div className={SERVER_FORM_BLOCK_STYLE}>
+          <input
+            className={SERVER_INPUT_STYLE}
+            onInput={(e) => setServer(e.currentTarget.value)}
+            placeholder="Server"
+            autoFocus
+          />
+          <input
+            className={SERVER_INPUT_STYLE}
+            onInput={(e) => {
+              const intVal = Number.parseInt(e.currentTarget.value);
+              if (!Number.isNaN(intVal) && Number.isInteger(intVal) && intVal < 65536) {
+                setPort(e.currentTarget.value);
+              }
+              else {
+                e.currentTarget.value = port;
+              }
+            }}
+            placeholder="Port"
+          />
+        </div>
+        <div className={SERVER_FORM_BLOCK_STYLE}>
+          <input
+            className={SERVER_INPUT_STYLE}
+            onInput={(e) => setNick(e.currentTarget.value)}
+            placeholder="Nickname"
+
+          />
+          <input
+            id="channels"
+            className={SERVER_INPUT_STYLE}
+            onInput={(e) => setChannels(e.currentTarget.value)}
+            placeholder="#nhex ##programming"
+          />
+        </div>
+        <div className="flex gap-2 justify-center flex-col ">
+          <div className={`${TLS_BUTTON_STYLE} ${tls ? TLS_ACTIVE_STYLE : TLS_INACTIVE_STYLE}`} onClick={handleTLS}>TLS</div>
+          <button
+            type="submit"
+            onClick={(e) => (e.currentTarget.textContent = 'Disconnect')} //needs wiring
+            className="border px-2 py-1"
+          >Connect</button>
+        </div>
+      </form>
+    </>
   )
 }
 
