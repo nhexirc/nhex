421167ad3bbcb5f45fa12b3ef703f49cb0709c71
Greg Pappas <gnpappas@proton.me>
2024-03-29 21:48:06 -0700

    Responsive styling tweaks

--- src/style.ts
+++ src/style.ts
@@ -2,26 +2,26 @@
 export const ROOT_STYLE = "flex flex-col bg-zinc-900 text-zinc-300 h-dvh scrollbar-thumb-zinc-400 scrollbar-track-zinc-700"
 
 //MENU
-export const MENU_STYLE = "flex gap-2 justify-center border-b mb-8 p-2"
+export const MENU_STYLE = "flex justify-center border-b mb-8 p-2"
 
 //MESSAGEBOX
 export const USER_INPUT = "w-full bg-zinc-900 border-b border-t p-1 focus:outline-none placeholder:font-bold placeholder-zinc-300"
 export const USERNAME_STYLE = "italic font-bold"
 export const USER_MESSAGE_STYLE = "pl-2"
-export const MESSAGE_BOX = "min-w-80 xl:w-[950px] lg:w-[730px] md:w-[480px] sm:w-[280px] sm:h-[600px] h-[500px] overflow-y-auto px-2 scrollbar-thin"
+export const MESSAGE_BOX = "w-96 xl:w-[950px] lg:w-[730px] md:w-[480px] sm:w-[400px] sm:h-[600px] h-[500px] overflow-y-auto px-2 scrollbar-thin overflow-x-hidden"
 
 //MAINVIEW
 export const IRC_STYLE = "max-w-8xl border mx-auto"
-export const CONNECT_STYLE = "max-w-8xl mx-auto bg-zinc-700 rounded-lg p-2"
+export const CONNECT_STYLE = "max-w-8xl mx-auto bg-zinc-700 rounded-lg p-2 min-w-80"
 export const SERVER_FORM_STYLE = "flex flex-col gap-2 p-2"
-export const SERVER_FORM_BLOCK_STYLE = "flex flex-col sm:flex-row gap-2"
+export const SERVER_FORM_BLOCK_STYLE = "flex flex-col gap-2"
 export const SERVER_INPUT_STYLE = "bg-zinc-800 placeholder-zinc-400 border p-1 focus:outline-none"
 
 //SERVER + MESSAGEBOX + NAMES
 export const SERV_MSG_NAMES_PANEL_STYLE = "flex flex-col sm:flex-row"
 
 //CHANNEL NAMES + SERVER WINDOW
-export const SERVER_CHAN_USER_PANEL_STYLE = "min-w-full md:min-w-32 sm:min-w-24 border-b px-2 overflow-x-hidden overflow-y-auto scrollbar-thin h-24 sm:h-[634px]"
+export const SERVER_CHAN_USER_PANEL_STYLE = "min-w-96 md:min-w-32 sm:min-w-24 border-b px-2 overflow-x-hidden overflow-y-auto scrollbar-thin h-24 sm:h-[634px]"
 
 //SERVER WINDOW
 export const SERVER_PANEL_STYLE = "border-r text-right"
