e97e685879e5008d809ffb402efe5f98fd43c489
Greg Pappas <gnpappas@proton.me>
2024-03-30 10:18:38 -0700

    The great exodus of borders

--- src/style.ts
+++ src/style.ts
@@ -2,10 +2,10 @@
 export const ROOT_STYLE = "flex flex-col bg-zinc-900 text-zinc-300 h-dvh scrollbar-thumb-zinc-400 scrollbar-track-zinc-700"
 
 //MENU
-export const MENU_STYLE = "flex justify-center border-b mb-8 gap-3 p-2"
+export const MENU_STYLE = "flex justify-center py-4 gap-3"
 export const MENU_LOGO = "bg-zinc-300 text-zinc-800 px-1 rounded hover:bg-zinc-200"
 //MESSAGEBOX
-export const USER_INPUT = "w-full bg-zinc-900 border-b border-t p-1 focus:outline-none placeholder:font-bold placeholder-zinc-300"
+export const USER_INPUT = "w-full bg-zinc-900 border-t p-1 focus:outline-none placeholder:font-bold placeholder-zinc-300"
 export const USERNAME_STYLE = "italic font-bold"
 export const USER_MESSAGE_STYLE = "pl-2"
 export const MESSAGE_BOX = "w-96 xl:w-[950px] lg:w-[730px] md:w-[480px] sm:w-[400px] sm:h-[600px] h-[500px] overflow-y-auto px-2 scrollbar-thin overflow-x-hidden"
@@ -21,7 +21,7 @@ export const SERVER_INPUT_STYLE = "bg-zinc-800 placeholder-zinc-400 border p-1 f
 export const SERV_MSG_NAMES_PANEL_STYLE = "flex flex-col sm:flex-row"
 
 //CHANNEL NAMES + SERVER WINDOW
-export const SERVER_CHAN_USER_PANEL_STYLE = "min-w-96 md:min-w-32 sm:min-w-24 border-b px-2 overflow-x-hidden overflow-y-auto scrollbar-thin h-24 sm:h-[634px]"
+export const SERVER_CHAN_USER_PANEL_STYLE = "min-w-96 md:min-w-32 sm:min-w-24  px-2 overflow-x-hidden overflow-y-auto scrollbar-thin h-24 sm:h-[634px]"
 
 //SERVER WINDOW
 export const SERVER_PANEL_STYLE = "border-r text-right"
@@ -30,7 +30,7 @@ export const SERVER_PANEL_STYLE = "border-r text-right"
 export const NAMES_PANEL_STYLE = "border-l"
 
 //FOOTER
-export const FOOTER_STYLE = "text-center p-2 border-t mt-8"
+export const FOOTER_STYLE = "text-center pt-4"
 
 //TLS STYLING
 export const TLS_ACTIVE_STYLE = `border-green-300 bg-green-800`
