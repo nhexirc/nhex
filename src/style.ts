182f8c1bb3368e23ba832b47b8720a523b76431f
Greg Pappas <gnpappas@proton.me>
2024-03-29 18:12:40 -0700

    Improved responsive base. Mobile view still needs work

--- src/style.ts
+++ src/style.ts
@@ -8,19 +8,20 @@ export const MENU_STYLE = "flex gap-2 justify-center border-b mb-8 p-2"
 export const USER_INPUT = "w-full bg-zinc-900 border-b border-t p-1 focus:outline-none placeholder:font-bold placeholder-zinc-300"
 export const USERNAME_STYLE = "italic font-bold"
 export const USER_MESSAGE_STYLE = "pl-2"
-export const MESSAGE_BOX = "md:min-w-96 md:h-[600px] overflow-y-auto px-2 scrollbar-thin"
+export const MESSAGE_BOX = "min-w-80 xl:w-[950px] lg:w-[730px] md:w-[480px] sm:w-[280px] sm:h-[600px] h-[500px] overflow-y-auto px-2 scrollbar-thin"
 
 //MAINVIEW
-export const MAINVIEW_STYLE = "max-w-6xl border mx-auto"
-export const SERVER_FORM_STYLE = "flex flex-col gap-2 border-b p-2"
-export const SERVER_FORM_BLOCK_STYLE = "flex flex-col md:flex-row gap-2"
+export const IRC_STYLE = "max-w-8xl border mx-auto"
+export const CONNECT_STYLE = "max-w-8xl mx-auto bg-zinc-700 rounded-lg p-2"
+export const SERVER_FORM_STYLE = "flex flex-col gap-2 p-2"
+export const SERVER_FORM_BLOCK_STYLE = "flex flex-col sm:flex-row gap-2"
 export const SERVER_INPUT_STYLE = "bg-zinc-800 placeholder-zinc-400 border p-1 focus:outline-none"
 
 //SERVER + MESSAGEBOX + NAMES
-export const SERV_MSG_NAMES_PANEL_STYLE = "flex flex-col md:flex-row"
+export const SERV_MSG_NAMES_PANEL_STYLE = "flex flex-col sm:flex-row"
 
 //CHANNEL NAMES + SERVER WINDOW
-export const SERVER_CHAN_USER_PANEL_STYLE = "min-w-48 border-b px-2 overflow-x-hidden overflow-y-auto scrollbar-thin h-24 md:h-[634px]"
+export const SERVER_CHAN_USER_PANEL_STYLE = "min-w-full md:min-w-32 sm:min-w-24 border-b px-2 overflow-x-hidden overflow-y-auto scrollbar-thin h-24 sm:h-[634px]"
 
 //SERVER WINDOW
 export const SERVER_PANEL_STYLE = "border-r text-right"
@@ -30,3 +31,8 @@ export const NAMES_PANEL_STYLE = "border-l"
 
 //FOOTER
 export const FOOTER_STYLE = "text-center p-2 border-t mt-8"
+
+//TLS STYLING
+export const TLS_ACTIVE_STYLE = `border-green-300 bg-green-800`
+export const TLS_INACTIVE_STYLE = `border-red-500 bg-red-800`
+export const TLS_BUTTON_STYLE = `border px-2 py-1 cursor-pointer select-none text-center`
