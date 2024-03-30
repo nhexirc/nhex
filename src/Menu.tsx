9dee8d1c46929cbaf6d0435b2fe77129df73a7a3
Greg Pappas <gnpappas@proton.me>
2024-03-30 09:49:39 -0700

    Add github logo to footer; adjust menu style and connect component layout

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,12 +1,13 @@
 import MenuItem from "./menu/MenuItem"
-import { MENU_STYLE } from "./style"
+import { MENU_LOGO, MENU_STYLE } from "./style"
 
 const Menu = () => {
 
-  const items = ["nhex", "view", "server", "settings", "window", "help"]
+  const items = ["view", "server", "settings", "window", "help"]
 
   return (
     <div className={MENU_STYLE}>
+      <a href="https://github.com/nhexirc/client" target="_blank" className={MENU_LOGO}>NHEX</a>
       {items.map((item, i) => <MenuItem key={i}>{item}</MenuItem>)}
     </div>
   )
