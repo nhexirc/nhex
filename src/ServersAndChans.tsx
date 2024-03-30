ae6483d1480930c221dd8cc31f1ba2d060aa4990
Greg Pappas <gnpappas@proton.me>
2024-03-29 21:49:03 -0700

    Rendering based on boolean state in emit functions. Almost works as intented; needs help beyond what I see in the code

--- src/ServersAndChans.tsx
+++ src/ServersAndChans.tsx
@@ -1,5 +1,6 @@
 import { emit } from '@tauri-apps/api/event';
 import { SERVER_CHAN_USER_PANEL_STYLE, SERVER_PANEL_STYLE } from './style';
+import { Dispatch, SetStateAction } from 'react';
 
 export type SACServers = Record<string, string[]>;
 export interface SACProps {
@@ -15,20 +16,26 @@ export interface SACSelectEvent {
   payload: SACSelect
 };
 
+const ServersAndChans = ({ servers, setIsServerSelected }: { servers: SACProps, setIsServerSelected: Dispatch<SetStateAction<boolean>> }) => {
 
-function emitSelect(server: string, channel: string = "") {
-  emit("nhex://servers_and_chans/select", { server, channel })
-}
+  function emitServer(server: string) {
+    setIsServerSelected(true);
+    emit("nhex://servers_and_chans/select", { server })
+  }
+  //this works better with server still attached, but it blows the server log out. needs further decoupling on a deeper level i am unfamiliar with -v
+  function emitChannel(server: string, channel: string = "") {
+    setIsServerSelected(false);
+    emit("nhex://servers_and_chans/select", { server, channel })
+  }
 
-const ServersAndChans = (props: SACProps) => {
   return (
     <div className={`${SERVER_CHAN_USER_PANEL_STYLE} ${SERVER_PANEL_STYLE}`}>
-      {Object.entries(props.servers).map(([serverName, chans]) => {
+      {Object.entries(servers).map(([serverName, chans]) => {
         return (
           <button id={`server_${serverName}`} className='text-right'>
-            <h3 className='font-bold' onClick={(e) => emitSelect(serverName)}>{serverName}</h3>
-            {chans.sort().map((channel) => (
-              <p id={`chan_${channel.replace("#", "_")}`} onClick={(e) => emitSelect(serverName, channel)}>
+            <h3 className='font-bold' onClick={() => emitServer(serverName)}>{serverName}</h3>
+            {chans.sort().map((channel: string) => (
+              <p id={`chan_${channel.replace("#", "_")}`} onClick={() => emitChannel(serverName, channel)}>
                 {channel}
               </p>
             ))}
