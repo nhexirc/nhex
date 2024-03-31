07f910aea7563d6ae0a313a60696cec30afcdcc6
terrablue <102580937+terrablue@users.noreply.github.com>
2024-03-31 16:43:03 +0200

    fix: provide a fallback for user settings in case file missing/faulty

--- src/lib/userSettings.ts
+++ src/lib/userSettings.ts
@@ -1,6 +1,23 @@
 import { createDir, exists, readTextFile, writeTextFile, BaseDirectory } from '@tauri-apps/api/fs';
 import { appConfigDir } from '@tauri-apps/api/path';
 import { parse, stringify, TomlPrimitive } from 'smol-toml';
+import defaultSettings from "./user-settings.default.json" assert { "type": "json" };
+
+// borrowed from https://github.com/rcompat/rcompat/blob/master/object/extend.js
+const extend = (base = {}, extension = {}) => {
+  if (typeof base !== "object") {
+    return base;
+  }
+  return Object.keys(extension).reduce((result, property) => {
+    const value = extension[property];
+    return {
+      ...result,
+      [property]: value?.constructor === Object
+        ? extend(base[property], value)
+        : value,
+    };
+  }, base);
+};
 
 const FILE_NAME = 'nhex.toml';
 
@@ -12,9 +29,15 @@ async function ensureOurAppConfigPath() {
 }
 
 export async function load(): Promise<Record<string, TomlPrimitive>> {
-    await ensureOurAppConfigPath();
-    const raw = await readTextFile(FILE_NAME, { dir: BaseDirectory.AppConfig });
-    return parse(raw);
+    let settings = {};
+    try {
+        await ensureOurAppConfigPath();
+        const raw = await readTextFile(FILE_NAME, { dir: BaseDirectory.AppConfig });
+        settings = parse(raw);
+    } catch (_) {
+        return defaultSettings;
+    }
+    return extend(defaultSettings, settings);
 }
 
 export async function save(obj: Record<string, any>) {
