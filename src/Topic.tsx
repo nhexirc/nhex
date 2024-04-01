6672bdacc20eb396a519a772f3973cef68e6a9c5
Ed Fletcher <ed@discordrc.com>
2024-04-01 15:06:41 -0700

    chore: use package.json version in placeholder topic

--- src/Topic.tsx
+++ src/Topic.tsx
@@ -1,5 +1,7 @@
 import { TOPIC_BAR, TOPIC_USER_INPUT } from "./style";
-const defaultTopic = "nhex v.0.1.2 :: nhex.dev :: irc.libera.chat #nhex"
+import { version } from '../package.json';
+
+const defaultTopic = `nhex v${version} :: nhex.dev :: irc.libera.chat #nhex`
 const Topic = ({ topic }: { topic: string }) => {
   return (
     <textarea value={!topic ? defaultTopic : topic} cols={1} disabled className={`${TOPIC_USER_INPUT} ${TOPIC_BAR}`}>
