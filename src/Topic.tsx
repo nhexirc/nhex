80ea5e7a9b7088fcdda3f76791d18785852e8afd
Greg Pappas <88697977+vxgxbxnd@users.noreply.github.com>
2024-03-31 18:28:08 -0700

    Topic (#74)
    * topic bar wip
    
    * Add textarea for malleability. Only one row shows w/ no overflow; traditional style
    
    * Styling
    
    * Revert topic to a div. Fixed height for one col of text; overflow hidden
    
    * Adjust styling for the topic bar
    
    * Topic bar element changed to textarea for malleability
    
    ---------
    
    Co-authored-by: Ed Fletcher <ed@discordrc.com>

---
+++ src/Topic.tsx
@@ -0,0 +1,10 @@
+import { TOPIC_BAR, TOPIC_USER_INPUT } from "./style";
+
+const Topic = ({ topic }) => {
+  return (
+    <textarea value={topic} cols={1} disabled className={`${TOPIC_USER_INPUT} ${TOPIC_BAR}`}>
+    </textarea>
+  )
+};
+
+export default Topic
