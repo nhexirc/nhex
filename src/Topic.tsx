2f920cebbd3cedefa19ada45eb3fd5978e72e499
Greg Pappas <gnpappas@proton.me>
2024-03-31 20:10:48 -0700

    Make message more concise

--- src/Topic.tsx
+++ src/Topic.tsx
@@ -1,5 +1,5 @@
 import { TOPIC_BAR, TOPIC_USER_INPUT } from "./style";
-const defaultTopic = "nhex v.0.1.1 :: visit us at https://nhex.dev or on libera.chat #nhex"
+const defaultTopic = "nhex v.0.1.1 :: [ https://nhex.dev ][ irc.libera.chat #nhex ]"
 const Topic = ({ topic }: { topic: string }) => {
   return (
     <textarea value={!topic ? defaultTopic : topic} cols={1} disabled className={`${TOPIC_USER_INPUT} ${TOPIC_BAR}`}>
