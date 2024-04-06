ef267df7dcbc21aa322718abe55746de4299d4c6
terrablue <102580937+terrablue@users.noreply.github.com>
2024-04-07 01:13:35 +0200

    fix: transform using dynamic template strings

--- src/lib/transformMessage.tsx
+++ src/lib/transformMessage.tsx
@@ -3,30 +3,25 @@ import {
   LINK_ELEMENT_STYLE
 } from "../style";
 
+const chunk = (strings, ...dynChunks) => strings.map((string, i) => <>
+    {string !== "" ? <span>{string}</span> : ""}
+    {dynChunks[i] !== undefined ? <span>
+        <a href={dynChunks[i]} target="_blank" className={LINK_ELEMENT_STYLE}>
+            {dynChunks[i]}
+        </a>
+    </span> : ""}
+</>);
+
 function jsxElementsFromMessage(message: string) {
-  let origMsg = message;
-  let messageElems = [<>{message}</>];
   const foundLinks = extract_urls(message);
 
-  if (foundLinks?.length) {
-    messageElems = foundLinks.map((l) => {
-      const linkDex = origMsg.indexOf(l);
-      const next = (<>
-        <span>{origMsg.slice(0, linkDex)}</span>
-        <span><a href={l} target="_blank" className={LINK_ELEMENT_STYLE}>{l}</a></span>
-      </>);
-      origMsg = origMsg.slice(linkDex + l.length);
-      return next;
-    });
-
-    // handle trailing text, if any
-    origMsg = origMsg.trim();
-    if (origMsg.trim().length) {
-      messageElems.push(<><span>{origMsg}</span></>);
-    }
+  if (foundLinks !== undefined) {
+    const prepared = foundLinks.reduce((message, l, i) =>
+        message.replace(l, `\$\{links[${i}]\}`), message);
+    return new Function("links", "chunk", `return chunk\`${prepared}\``)(foundLinks, chunk);
   }
 
-  return messageElems;
+  return [<>{message}</>];
 }
 
 export default function(message: string) {
