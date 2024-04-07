b2f66ffc1255eb8bc194e7653de5966afba2e2e1
Greg Pappas <gnpappas@proton.me>
2024-04-06 19:56:06 -0700

    Adjust so styles are mostly modified by base colors

--- src/style.ts
+++ src/style.ts
@@ -1,8 +1,8 @@
 //APP
-export const ROOT_STYLE = "flex flex-col  h-dvh scrollbar-thumb-zinc-400 scrollbar-track-zinc-700"
+export const ROOT_STYLE = "flex flex-col h-dvh scrollbar-thumb-zinc-400 scrollbar-track-zinc-700"
 export const NIGHT_STYLE = "bg-slate-900 text-stone-300"
-export const DAY_STYLE = "bg-orange-300/40 text-black"
-
+export const DAY_STYLE = "bg-orange-400/30 text-black"
+export const UNIFORM_BORDER_STYLE = "border-zinc-500"
 //MENU
 export const MENU_STYLE = "flex items-center justify-center py-2 gap-5"
 
@@ -13,27 +13,27 @@ export const MESSAGE_BOX = "bg-zinc-600/30 font-mono w-96 xl:w-[950px] lg:w-[730
 export const JOIN_PART_MSG = "italic"
 export const JOIN_PART_MSG_DIM = "text-zinc-500"
 export const TIMESTAMP_STYLE = "pr-2 text-zinc-500"
-export const LINK_ELEMENT_STYLE = "text-sky-500"
+export const LINK_ELEMENT_STYLE = "text-sky-600"
 
 //IRC COMPONENT
 export const IRC_STYLE = "max-w-7xl mx-auto"
 export const IRC_CONDITIONAL_STYLE = "flex flex-col gap-2"
 
 //CONNECT COMPONENT
-export const CONNECT_STYLE = "max-w-7xl mx-auto bg-zinc-500/30 rounded-lg p-2 min-w-[420px]"
+export const CONNECT_STYLE = "max-w-7xl mx-auto bg-zinc-500/30 rounded-lg p-2 min-w-[420px] shadow-xl"
 export const SERVER_FORM_STYLE = "flex flex-col gap-2 p-2"
 export const SERVER_FORM_BLOCK_STYLE = "flex flex-col gap-2"
-export const SERVER_INPUT_STYLE = "bg-zinc-500/50 placeholder-zinc-950/50 p-1 focus:outline-none rounded"
+export const SERVER_INPUT_STYLE = "bg-zinc-500/50 placeholder-zinc-200/50 p-1 focus:outline-none rounded"
 export const SERVER_CONNECT_BUTTON = "border px-2 py-1 bg-blue-500/50 border-blue-100/90 font-bold rounded"
-export const TLS_ACTIVE_STYLE = `border-green-300 bg-green-700/70`
-export const TLS_INACTIVE_STYLE = `border-red-500 bg-red-700/70`
+export const TLS_ACTIVE_STYLE = `bg-green-600`
+export const TLS_INACTIVE_STYLE = `bg-red-600`
 export const TLS_BUTTON_STYLE = `px-2 py-1 cursor-pointer select-none text-center rounded`
 
 //TOPIC + USER INPUT
 export const TOPIC_USER_INPUT = "w-96 sm:w-full sm:max-w-[635px] md:max-w-full border bg-zinc-500/50 p-1"
 
 //TOPIC
-export const TOPIC_BAR = "italic h-8 resize-none text-center scrollbar-thin overflow-auto";
+export const TOPIC_BAR = "italic h-8 resize-none text-center scrollbar-thin overflow-y-auto overflow-x-hidden";
 
 //USER INPUT COMPONENT
 export const USER_INPUT = "focus:outline-none"
@@ -42,7 +42,7 @@ export const USER_INPUT = "focus:outline-none"
 export const SERV_MSG_NAMES_PANEL_STYLE = "flex flex-col sm:flex-row mx-auto"
 
 //CHANNEL NAMES + SERVER WINDOW
-export const SERVER_NAMES_PANEL_STYLE = "bg-zinc-700/60 w-full sm:min-w-32 sm:w-32 px-2 overflow-x-auto overflow-y-auto scrollbar-thin h-24 sm:h-[600px] border-x border-y sm:border-b"
+export const SERVER_NAMES_PANEL_STYLE = "bg-zinc-600/60 w-full sm:min-w-32 sm:w-32 px-2 overflow-x-auto overflow-y-auto scrollbar-thin h-24 sm:h-[600px] border-x border-y sm:border-b"
 
 //SERVER
 export const SERVER_PANEL_STYLE = "text-right"
