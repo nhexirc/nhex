1d4d4de908ebaf93baf88c438ba836af1e58d425
Greg Pappas <gnpappas@proton.me>
2024-04-06 17:19:07 -0700

    Add day night theme toggle; state in App so children have access. Potential context refactor

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,13 +1,13 @@
 import MenuItem from "./menu/MenuItem";
 import { MENU_STYLE, NHEX } from "./style";
-
-const Menu = () => {
+const Menu = ({ dayNightToggle, isNight }) => {
   const items = ["view", "server", "settings", "window", "help"];
 
   return (
     <div className={MENU_STYLE}>
       <a href="https://github.com/nhexirc/client" target="_blank" className={NHEX}>nhex</a>
       {items.map((item, i) => <MenuItem key={i}>{item}</MenuItem>)}
+      <button onClick={dayNightToggle}>{isNight ? "night" : "day"}</button>
     </div>
   );
 }
