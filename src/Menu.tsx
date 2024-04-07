aa14b68bd051b7635173f63e0ff9c5eabb1c9908
Greg Pappas <gnpappas@proton.me>
2024-04-06 17:50:54 -0700

    Use svg's to represent theme to switch to after clicking

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,5 +1,7 @@
 import MenuItem from "./menu/MenuItem";
 import { MENU_STYLE, NHEX } from "./style";
+import sun from "./assets/sun.svg"
+import moon from "./assets/moon.svg"
 const Menu = ({ dayNightToggle, isNight }) => {
   const items = ["view", "server", "settings", "window", "help"];
 
@@ -7,7 +9,7 @@ const Menu = ({ dayNightToggle, isNight }) => {
     <div className={MENU_STYLE}>
       <a href="https://github.com/nhexirc/client" target="_blank" className={NHEX}>nhex</a>
       {items.map((item, i) => <MenuItem key={i}>{item}</MenuItem>)}
-      <button onClick={dayNightToggle}>{isNight ? "night" : "day"}</button>
+      <button onClick={dayNightToggle}><img className="w-8" src={isNight ? sun : moon} alt="day/night toggle" /></button>
     </div>
   );
 }
