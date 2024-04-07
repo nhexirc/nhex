abdc31713edac96850569a50f3aca22a99710d92
Greg Pappas <gnpappas@proton.me>
2024-04-07 14:04:15 -0700

    Adjust Connect styling for normal and mobile view

--- src/style.ts
+++ src/style.ts
@@ -4,6 +4,22 @@ export const NIGHT_STYLE = "bg-slate-900 text-stone-300"
 export const DAY_STYLE = "bg-orange-400/30 text-black"
 export const UNIFORM_BORDER_STYLE = "border-zinc-500"
 
+//MENU
+export const MENU_STYLE = "flex flex-row items-center justify-center py-2 gap-4"
+
+//IRC COMPONENT
+export const IRC_STYLE = "flex flex-col px-4"
+
+//CONNECT COMPONENT
+export const CONNECT_STYLE = "flex flex-col mx-auto items-center justify-center max-w-7xl bg-zinc-500/30 rounded-lg p-2 h-full w-full sm:w-3/4 shadow-xl"
+export const SERVER_FORM_STYLE = "flex flex-col gap-2 p-2 w-3/4 sm:w-96"
+export const SERVER_FORM_BLOCK_STYLE = "flex flex-col gap-2"
+export const SERVER_INPUT_STYLE = "bg-zinc-500/50 placeholder-zinc-200/50 p-1 focus:outline-none rounded"
+export const SERVER_CONNECT_BUTTON = "border px-2 py-1 bg-blue-500/50 border-blue-100/90 font-bold rounded"
+export const TLS_ACTIVE_STYLE = `bg-green-600`
+export const TLS_INACTIVE_STYLE = `bg-red-600`
+export const TLS_BUTTON_STYLE = `px-2 py-1 cursor-pointer select-none text-center rounded`
+
 //MESSAGEBOX
 export const USERNAME_STYLE = "italic font-bold"
 export const GLOBAL_MESSAGE_STYLE = "pl-2"
@@ -12,20 +28,6 @@ export const JOIN_PART_MSG = "italic"
 export const JOIN_PART_MSG_DIM = "text-zinc-500"
 export const TIMESTAMP_STYLE = "pr-2 text-zinc-500"
 export const LINK_ELEMENT_STYLE = "text-sky-600"
-
-//IRC COMPONENT
-export const IRC_STYLE = "flex flex-col px-4"
-
-//CONNECT COMPONENT
-export const MENU_STYLE = "flex flex-row items-center justify-center py-2 gap-4"
-export const CONNECT_STYLE = "max-w-7xl mx-auto bg-zinc-500/30 rounded-lg p-2 mt-2 sm:min-w-[420px] shadow-xl"
-export const SERVER_FORM_STYLE = "flex flex-col gap-2 p-2"
-export const SERVER_FORM_BLOCK_STYLE = "flex flex-col gap-2"
-export const SERVER_INPUT_STYLE = "bg-zinc-500/50 placeholder-zinc-200/50 p-1 focus:outline-none rounded"
-export const SERVER_CONNECT_BUTTON = "border px-2 py-1 bg-blue-500/50 border-blue-100/90 font-bold rounded"
-export const TLS_ACTIVE_STYLE = `bg-green-600`
-export const TLS_INACTIVE_STYLE = `bg-red-600`
-export const TLS_BUTTON_STYLE = `px-2 py-1 cursor-pointer select-none text-center rounded`
 
 //TOPIC + USER INPUT
 export const TOPIC_USER_INPUT = "border bg-zinc-500/50 p-1"
