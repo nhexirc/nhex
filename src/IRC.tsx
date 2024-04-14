9ae7fd19ba3874bee77e239fcf6875bb1840a5fb
Greg Pappas <gnpappas@proton.me>
2024-04-14 12:07:17 -0700

    Move topic bar in MessageBox

--- src/IRC.tsx
+++ src/IRC.tsx
@@ -3,23 +3,21 @@ import MessageBox from "./MessageBox"
 import ChannelNames from "./ChannelNames"
 import { IRC_STYLE, SERV_MSG_NAMES_PANEL_STYLE } from "./style"
 import { useState } from "react"
-import Topic from "./Topic";
 
 const IRC = ({ servers, names, message, nick, isNight, settings, topic, getCurSelection, getBuffers, STATE }) => {
   const [isServerSelected, setIsServerSelected] = useState(true);
   return (
     <div className={IRC_STYLE}>
-      <Topic topic={topic} />
 
       {isServerSelected ?
         <div className={SERV_MSG_NAMES_PANEL_STYLE}>
           <ServersAndChans setIsServerSelected={setIsServerSelected} servers={servers} getCurSelection={getCurSelection} getBuffers={getBuffers} />
-          <MessageBox lines={message} settings={settings} STATE={STATE} nick={nick} isNight={isNight} />
+          <MessageBox lines={message} settings={settings} STATE={STATE} nick={nick} isNight={isNight} topic={topic} />
         </div>
         :
         <div className={SERV_MSG_NAMES_PANEL_STYLE}>
           <ServersAndChans setIsServerSelected={setIsServerSelected} servers={servers} getCurSelection={getCurSelection} getBuffers={getBuffers} />
-          <MessageBox lines={message} settings={settings} STATE={STATE} nick={nick} isNight={isNight} />
+          <MessageBox lines={message} settings={settings} STATE={STATE} nick={nick} isNight={isNight} topic={topic} />
           <ChannelNames names={names} />
         </div>
       }
