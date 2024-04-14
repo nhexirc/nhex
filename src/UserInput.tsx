f916a71ede245e6f410c52e32e9324b33996098a
Greg Pappas <gnpappas@proton.me>
2024-04-13 19:58:59 -0700

    Code compartmentalization; placeholder text adjustment

--- src/UserInput.tsx
+++ src/UserInput.tsx
@@ -4,30 +4,30 @@ import { completeNickname } from "./MainView";
 import { parseMBUserInputRaw } from './lib/common';
 
 const UserInput = ({ nick, isNight }) => {
-
+  const inputFunctionality = (e: any) => {
+    if (e.key === "Tab") {
+      const [first, ...rest] = e.currentTarget.value.split(" ");
+      if (prefix === "") {
+        prefix = first;
+      }
+      const completed = completeNickname(prefix, first);
+      e.currentTarget.value = [completed,
+        ...rest.filter(r => r !== "")].join(" ")
+      e.preventDefault();
+      // prevent reseting prefix, for cycling through
+      return;
+    }
+    // reset prefix
+    prefix = "";
+    if (e.key === "Enter") {
+      const userInput = parseMBUserInputRaw(e.currentTarget.value);
+      e.currentTarget.value = "";
+      emit("nhex://user_input/raw", userInput).catch(console.error);
+    }
+  }
   let prefix = "";
   return (
-    <input type="text" className={`${TOPIC_USER_INPUT} ${UNIFORM_BORDER_STYLE} ${isNight ? `placeholder:text-white/60 text-white/90` : `placeholder:text-black/60 text-black`} focus:outline-none`} autoFocus placeholder={nick} onKeyDown={(e) => {
-      if (e.key === "Tab") {
-        const [first, ...rest] = e.currentTarget.value.split(" ");
-        if (prefix === "") {
-          prefix = first;
-        }
-        const completed = completeNickname(prefix, first);
-        e.currentTarget.value = [completed,
-          ...rest.filter(r => r !== "")].join(" ")
-        e.preventDefault();
-        // prevent reseting prefix, for cycling through
-        return;
-      }
-      // reset prefix
-      prefix = "";
-      if (e.key === "Enter") {
-        const userInput = parseMBUserInputRaw(e.currentTarget.value);
-        e.currentTarget.value = "";
-        emit("nhex://user_input/raw", userInput).catch(console.error);
-      }
-    }} />
+    <input type="text" className={`${TOPIC_USER_INPUT} ${UNIFORM_BORDER_STYLE} ${isNight ? `placeholder:text-white/70 text-white/90` : `placeholder:text-black/70 text-black`} focus:outline-none`} autoFocus placeholder={nick} onKeyDown={inputFunctionality} />
   )
 }
 
