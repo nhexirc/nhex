d7628d8d04c11b289818f3536adecdc5e70f809f
Greg Pappas <gnpappas@proton.me>
2024-04-14 12:54:19 -0700

    Add greater theme control for topic and user input

--- src/IRC.tsx
+++ src/IRC.tsx
@@ -4,7 +4,7 @@ import ChannelNames from "./ChannelNames"
 import { IRC_STYLE, SERV_MSG_NAMES_PANEL_STYLE } from "./style"
 import { useState } from "react"
 
-const IRC = ({ servers, names, message, nick, isNight, settings, topic, getCurSelection, getBuffers, STATE }) => {
+const IRC = ({ servers, names, message, nick, isNight, dayNightToggle, settings, topic, getCurSelection, getBuffers, STATE }) => {
   const [isServerSelected, setIsServerSelected] = useState(true);
   return (
     <div className={IRC_STYLE}>
@@ -12,12 +12,12 @@ const IRC = ({ servers, names, message, nick, isNight, settings, topic, getCurSe
       {isServerSelected ?
         <div className={SERV_MSG_NAMES_PANEL_STYLE}>
           <ServersAndChans setIsServerSelected={setIsServerSelected} servers={servers} getCurSelection={getCurSelection} getBuffers={getBuffers} />
-          <MessageBox lines={message} settings={settings} STATE={STATE} nick={nick} isNight={isNight} topic={topic} />
+          <MessageBox lines={message} settings={settings} STATE={STATE} nick={nick} isNight={isNight} topic={topic} dayNightToggle={dayNightToggle} />
         </div>
         :
         <div className={SERV_MSG_NAMES_PANEL_STYLE}>
           <ServersAndChans setIsServerSelected={setIsServerSelected} servers={servers} getCurSelection={getCurSelection} getBuffers={getBuffers} />
-          <MessageBox lines={message} settings={settings} STATE={STATE} nick={nick} isNight={isNight} topic={topic} />
+          <MessageBox lines={message} settings={settings} STATE={STATE} nick={nick} isNight={isNight} topic={topic} dayNightToggle={dayNightToggle} />
           <ChannelNames names={names} />
         </div>
       }
