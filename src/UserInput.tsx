1364233a0734dbcdd78ed3232276361d9ed5e7f5
Greg Pappas <gnpappas@proton.me>
2024-04-13 19:44:00 -0700

    Style text depending on day/night theme.

--- src/UserInput.tsx
+++ src/UserInput.tsx
@@ -1,13 +1,13 @@
 import { emit } from "@tauri-apps/api/event";
-import { TOPIC_USER_INPUT, UNIFORM_BORDER_STYLE, USER_INPUT } from "./style";
+import { TOPIC_USER_INPUT, UNIFORM_BORDER_STYLE } from "./style";
 import { completeNickname } from "./MainView";
 import { parseMBUserInputRaw } from './lib/common';
 
-const UserInput = () => {
+const UserInput = ({ nick, isNight }) => {
 
   let prefix = "";
   return (
-    <input type="text" className={`${TOPIC_USER_INPUT} ${USER_INPUT} ${UNIFORM_BORDER_STYLE}`} autoFocus onKeyDown={(e) => {
+    <input type="text" className={`${TOPIC_USER_INPUT} ${UNIFORM_BORDER_STYLE} ${isNight ? `placeholder:text-white/60 text-white/90` : `placeholder:text-black/60 text-black`} focus:outline-none`} autoFocus placeholder={nick} onKeyDown={(e) => {
       if (e.key === "Tab") {
         const [first, ...rest] = e.currentTarget.value.split(" ");
         if (prefix === "") {
