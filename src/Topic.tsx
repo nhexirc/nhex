dbc9d7fc75f84b9e0ee18f095e202ce1b45024c0
Greg Pappas <gnpappas@proton.me>
2024-04-14 12:56:15 -0700

    Remove type

--- src/Topic.tsx
+++ src/Topic.tsx
@@ -2,7 +2,7 @@ import { TOPIC_BAR, TOPIC_USER_INPUT, TOPIC_USER_THEME_DAY, TOPIC_USER_THEME_NIG
 import { version } from '../package.json';
 
 const defaultTopic = `nhex v${version} :: nhex.dev :: irc.libera.chat #nhex`
-const Topic = ({ topic, isNight }: { topic: string }) => {
+const Topic = ({ topic, isNight }) => {
   return (
     <textarea value={!topic ? defaultTopic : topic} cols={1} disabled className={`${TOPIC_USER_INPUT} ${TOPIC_BAR} ${UNIFORM_BORDER_STYLE} ${isNight ? TOPIC_USER_THEME_NIGHT : TOPIC_USER_THEME_DAY} sticky top-0 w-full`}>
     </textarea>
