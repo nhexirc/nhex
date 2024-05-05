768f1830dc2094a4ac78d121bbb43ff1669d0de5
Greg P. <88697977+zvxvx@users.noreply.github.com>
2024-05-04 17:43:37 -0700

    Clean up DND; make more concise

--- packages/client/src/settings/DNDSettings.tsx
+++ packages/client/src/settings/DNDSettings.tsx
@@ -1,45 +1,37 @@
+import { SETTINGS_ALIGNMENT, SETTINGS_HEADER } from '../style';
 import BoolSetting from './BoolSetting';
 import TextSetting from './TextSetting';
 
-export default function ({ isNight, settings, setSettings }) {
+export default function({ isNight, settings, setSettings }) {
   return (
-    <>
-      <div className="text-2xl mb-4">Drag-and-Drop</div>
-      <div>
-        <table className="w-full">
-          <tbody>
-            <BoolSetting
-              displayName="Enabled"
-              sectionName="DragAndDrop"
-              fieldName="enable"
-              isNight={isNight}
-              settings={settings}
-              setSettings={setSettings}>
-            </BoolSetting>
-
-            <TextSetting
-              displayName="Allowed text file extensions"
-              sectionName="DragAndDrop"
-              fieldName="textFileExtensions"
-              isNight={isNight}
-              settings={settings}
-              setSettings={setSettings}
-              displayXform={(l) => l?.join(" ")}
-              valueXform={(s) => s?.split(" ")}
-            >
-            </TextSetting>
-
-            <TextSetting
-              displayName="Upload host"
-              sectionName="DragAndDrop"
-              fieldName="textUploadHost"
-              isNight={isNight}
-              settings={settings}
-              setSettings={setSettings}>
-            </TextSetting>
-          </tbody>
-        </table>
-      </div>
-    </>
+    <div className="flex flex-col gap-2">
+      <BoolSetting
+        sectionName="DragAndDrop"
+        fieldName="enable"
+        isNight={isNight}
+        settings={settings}
+        setSettings={setSettings}>
+        <p className={SETTINGS_HEADER}>Drag and Drop</p>
+      </BoolSetting>
+      <TextSetting
+        displayName="Allowed text file extensions"
+        sectionName="DragAndDrop"
+        fieldName="textFileExtensions"
+        isNight={isNight}
+        settings={settings}
+        setSettings={setSettings}
+        displayXform={(l) => l?.join(" ")}
+        valueXform={(s) => s?.split(" ")}
+      >
+      </TextSetting>
+      <TextSetting
+        displayName="Upload host"
+        sectionName="DragAndDrop"
+        fieldName="textUploadHost"
+        isNight={isNight}
+        settings={settings}
+        setSettings={setSettings}>
+      </TextSetting>
+    </div>
   );
 }
