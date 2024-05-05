d11d3318e494e6309ef2d9fd1ad4debc31cfd4e4
Greg P. <88697977+zvxvx@users.noreply.github.com>
2024-05-04 19:20:15 -0700

    Remove bold on selected menu item

--- packages/client/src/menu/MenuItem.tsx
+++ packages/client/src/menu/MenuItem.tsx
@@ -2,7 +2,7 @@ import { emit } from '@tauri-apps/api/event';
 
 const MenuItem = ({ children, triggerFunc, state }) => {
   return (
-    <button className={(state?.[children] && "underline font-bold")}>
+    <button className={(state?.[children] && "underline")}>
       <a href="#" onClick={triggerFunc}>
         {children}
       </a>
