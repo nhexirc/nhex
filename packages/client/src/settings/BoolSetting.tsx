1b70446e58c77c559456e80be99b05e44c4ade7b
Greg P. <88697977+zvxvx@users.noreply.github.com>
2024-05-04 17:43:06 -0700

    Update styling for all checkboxes

--- packages/client/src/settings/BoolSetting.tsx
+++ packages/client/src/settings/BoolSetting.tsx
@@ -1,27 +1,23 @@
 import UserSettings from '../lib/userSettings';
-import { DAY_STYLE, NIGHT_STYLE, USER_INPUT_CB } from "../style";
+import { DAY_STYLE, NIGHT_STYLE, SETTINGS_ALIGNMENT, USER_INPUT_CHECKBOX } from "../style";
 
-export default function({ displayName, sectionName, fieldName, isNight, settings, setSettings }) {
+export default function({ children, sectionName, fieldName, isNight, settings, setSettings }) {
   return (
-    <>
-      <tr>
-        <td>{displayName}:</td>
-        <td>
-          <input
-            className={`${isNight ? NIGHT_STYLE : DAY_STYLE} ${USER_INPUT_CB}`}
-            type="checkbox"
-            checked={settings?.[sectionName]?.[fieldName]}
-            onChange={(e) => {
-              UserSettings.save({
-                ...settings,
-                [sectionName]: {
-                  ...settings?.[sectionName],
-                  [fieldName]: e.target.checked
-                }
-              }).then((newSettings) => setSettings(newSettings));
-            }} />
-        </td>
-      </tr>
-    </>
+    <div className={SETTINGS_ALIGNMENT}>
+      <input
+        className={`${isNight ? NIGHT_STYLE : DAY_STYLE} ${USER_INPUT_CHECKBOX}`}
+        type="checkbox"
+        checked={settings?.[sectionName]?.[fieldName]}
+        onChange={(e) => {
+          UserSettings.save({
+            ...settings,
+            [sectionName]: {
+              ...settings?.[sectionName],
+              [fieldName]: e.target.checked
+            }
+          }).then((newSettings) => setSettings(newSettings));
+        }} />
+      <p>{children}</p>
+    </div>
   );
 }
