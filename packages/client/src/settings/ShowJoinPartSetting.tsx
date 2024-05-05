d23d8c262ba95f65859225081972dde2cc55e0a9
Greg P. <88697977+zvxvx@users.noreply.github.com>
2024-05-04 18:16:41 -0700

    Make j/p settings display more concise

--- packages/client/src/settings/ShowJoinPartSetting.tsx
+++ packages/client/src/settings/ShowJoinPartSetting.tsx
@@ -1,29 +1,26 @@
 import UserSettings from '../lib/userSettings';
-import { DAY_STYLE, NIGHT_STYLE, USER_INPUT_CHECKBOX } from "../style";
+import { DAY_STYLE, NIGHT_STYLE, SETTINGS_ALIGNMENT, USER_INPUT_CHECKBOX } from "../style";
 
 export default function({ which, isNight, settings, setSettings }) {
+  const displayText = which[0].toUpperCase() + which.slice(1) + 's'
   return (
-    <>
-      <tr>
-        <td>Show {which}s:</td>
-        <td>
-          <input
-            className={`${isNight ? NIGHT_STYLE : DAY_STYLE} ${USER_INPUT_CHECKBOX}`}
-            type="checkbox"
-            checked={settings?.MessageBox?.show.includes(which)}
-            onChange={(e) => {
-              const newShowSet = new Set(settings?.MessageBox?.show);
-              newShowSet[e.target.checked ? "add" : "delete"](which);
-              UserSettings.save({
-                ...settings,
-                MessageBox: {
-                  ...settings.MessageBox,
-                  show: [...newShowSet]
-                }
-              }).then((newSettings) => setSettings(newSettings));
-            }} />
-        </td>
-      </tr>
-    </>
+    <div className={SETTINGS_ALIGNMENT}>
+      <input
+        className={`${isNight ? NIGHT_STYLE : DAY_STYLE} ${USER_INPUT_CHECKBOX}`}
+        type="checkbox"
+        checked={settings?.MessageBox?.show.includes(which)}
+        onChange={(e) => {
+          const newShowSet = new Set(settings?.MessageBox?.show);
+          newShowSet[e.target.checked ? "add" : "delete"](which);
+          UserSettings.save({
+            ...settings,
+            MessageBox: {
+              ...settings.MessageBox,
+              show: [...newShowSet]
+            }
+          }).then((newSettings) => setSettings(newSettings));
+        }} />
+      <p>{displayText}</p>
+    </div>
   );
 }
