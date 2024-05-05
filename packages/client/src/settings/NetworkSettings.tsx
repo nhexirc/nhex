1167065514a8d504d07cda2a59773298f49e983e
Greg P. <88697977+zvxvx@users.noreply.github.com>
2024-05-04 18:38:11 -0700

    Fix up Network... design

--- packages/client/src/settings/NetworkSettings.tsx
+++ packages/client/src/settings/NetworkSettings.tsx
@@ -1,61 +1,52 @@
 import TextSetting from './TextSetting';
 import BoolSetting from './BoolSetting';
+import { SETTINGS_HEADER } from '../style';
 
 const NetworkSettings = ({ isNight, settings, setSettings }) => {
   return (
-    <>
-      <div className="text-2xl mb-4">Network</div>
-      <div>
-        <table className="w-full">
-          <tbody>
-            <TextSetting
-              displayName="Server"
-              sectionName="Network"
-              fieldName="server"
-              isNight={isNight}
-              settings={settings}
-              setSettings={setSettings}>
-            </TextSetting>
-
-            <TextSetting
-              displayName="Port"
-              sectionName="Network"
-              fieldName="port"
-              isNight={isNight}
-              settings={settings}
-              setSettings={setSettings}>
-            </TextSetting>
-
-            <TextSetting
-              displayName="Nickname"
-              sectionName="Network"
-              fieldName="nick"
-              isNight={isNight}
-              settings={settings}
-              setSettings={setSettings}>
-            </TextSetting>
-
-            <TextSetting
-              displayName="Channels"
-              sectionName="Network"
-              fieldName="channels"
-              isNight={isNight}
-              settings={settings}
-              setSettings={setSettings}>
-            </TextSetting>
-
-            <BoolSetting
-              displayName="Use TLS"
-              sectionName="Network"
-              fieldName="tls"
-              isNight={isNight}
-              settings={settings}
-              setSettings={setSettings}>
-            </BoolSetting>
-          </tbody>
-        </table>
-      </div>
-    </>
+    <div className='flex flex-col gap-2'>
+      <div className={SETTINGS_HEADER}>Network</div>
+      <TextSetting
+        displayName="Server"
+        sectionName="Network"
+        fieldName="server"
+        isNight={isNight}
+        settings={settings}
+        setSettings={setSettings}>
+      </TextSetting>
+      <TextSetting
+        displayName="Port"
+        sectionName="Network"
+        fieldName="port"
+        isNight={isNight}
+        settings={settings}
+        setSettings={setSettings}>
+      </TextSetting>
+      <TextSetting
+        displayName="Nickname"
+        sectionName="Network"
+        fieldName="nick"
+        isNight={isNight}
+        settings={settings}
+        setSettings={setSettings}>
+      </TextSetting>
+      <TextSetting
+        displayName="Channels"
+        sectionName="Network"
+        fieldName="channels"
+        isNight={isNight}
+        settings={settings}
+        setSettings={setSettings}>
+      </TextSetting>
+      <BoolSetting
+        sectionName="Network"
+        fieldName="tls"
+        isNight={isNight}
+        settings={settings}
+        setSettings={setSettings}>
+        TLS
+      </BoolSetting>
+    </div>
   );
 }
 export default NetworkSettings
